955a13698b3f0b45fbabb44eebe61bdb8fc672a1
Plena03 <95049600+Plena03@users.noreply.github.com>
2022-08-23 16:54:16 +0400

    Изменения

--- DZ_N25/Program.cs
+++ DZ_N25/Program.cs
@@ -2,6 +2,29 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16 
 
+// Решение через функцию
+
+//Console.WriteLine("Программа возводит число А в натуральную степень В.");
+
+//Console.WriteLine("Введите число A:");
+//var a = int.Parse(Console.ReadLine()!);
+
+//Console.WriteLine("Введите число B:");
+//var b = int.Parse(Console.ReadLine()!);
+
+//long Degree (long num)
+//{   
+//    var result =(long) Math.Pow(a, b);
+//    return result;
+//}
+//var num = 1;
+
+//var result = Degree(num);//number
+
+//Console.WriteLine("Результат:" + result);
+
+//Решение через цикл
+
 Console.WriteLine("Программа возводит число А в натуральную степень В.");
 
 Console.WriteLine("Введите число A:");
@@ -10,13 +33,12 @@ var a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите число B:");
 var b = int.Parse(Console.ReadLine()!);
 
-long Degree (long num)
-{   
-    var result =(long) Math.Pow(a, b);
-    return result;
+int x = 1;
+int sum = a;
+
+while (x != b)
+{
+    sum *= a;
+    x += 1;
 }
-var num = 1;
-
-var result = Degree(num);//number
-
-Console.WriteLine("Результат:" + result);
+Console.WriteLine("Результат: " + sum);
